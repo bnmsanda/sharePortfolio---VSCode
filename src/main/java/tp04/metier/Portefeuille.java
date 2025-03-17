4233640e567e92bb60de2777cea85a1cf2673fd7
HuangjingL <151631854+Bonjoureloi@users.noreply.github.com>
2025-03-17 14:42:23 +0100

    Update Portefeuille.java

--- src/main/java/tp04/metier/Portefeuille.java
+++ src/main/java/tp04/metier/Portefeuille.java
@@ -15,76 +15,29 @@
  */
 package tp04.metier;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
 
-/**
- *
- * @author perussel
- */
 public class Portefeuille {
 
-    Map<Action, LignePortefeuille> mapLignes;
+	ArrayList<Action> myActions;
 
-    private class LignePortefeuille {
+	public Portefeuille() {
+		this.myActions = new ArrayList<Action>();
+	}
 
-        private Action action;
+	public void buyNewAction(String libelle) {
+		this.myActions.add(new ActionSimple(libelle));
+	}
 
-        private int qte;
+	public void buyExistingAction(Action a) {
+		this.myActions.add(a);
+	}
 
-        public int getQte() {
-            return qte;
-        }
+	public double sellAction(Action a, int year, int day) throws Exception {
+		double value = this.myActions.get(this.myActions.indexOf(a)).getValue(day, year);
+		this.myActions.remove(a);
+		return value;
+	}
 
-        public void setQte(int qte) {
-            this.qte = qte;
-        }
-
-        public Action getAction() {
-            return this.action;
-        }
-
-        public LignePortefeuille(Action action, int qte) {
-            this.action = action;
-            this.qte = qte;
-        }
-
-        public String toString() {
-            return Integer.toString(qte);
-        }
-    }
-
-    public Portefeuille() {
-        this.mapLignes = new HashMap();
-    }
-
-    public void acheter(Action a, int q) {
-        if (this.mapLignes.containsKey(a) == false) {
-            this.mapLignes.put(a, new LignePortefeuille(a, q));
-        } else {
-            this.mapLignes.get(a).setQte(this.mapLignes.get(a).getQte() + q);
-        }
-    }
-
-    public void vendre(Action a, int q) {
-        if (this.mapLignes.containsKey(a) == true) {
-            if (this.mapLignes.get(a).getQte() > q) {
-                this.mapLignes.get(a).setQte(this.mapLignes.get(a).getQte() - q);
-            } else if (this.mapLignes.get(a).getQte() == q) {
-                this.mapLignes.remove(a);
-            }
-        }
-    }
-
-    public String toString() {
-        return this.mapLignes.toString();
-    }
-
-    public float valeur(Jour j) {
-        float total = 0;
-        for (LignePortefeuille lp : this.mapLignes.values()) {
-            total = total + (lp.getQte() * lp.getAction().valeur(j));
-        }
-        return total;
-    }
 }
+
