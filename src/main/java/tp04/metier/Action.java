3d96c0e72a8995324d7224d23b80e5f02c6cc4a1
Bonjoureloi <151631854+Bonjoureloi@users.noreply.github.com>
2025-03-17 15:41:24 +0100

    push

--- src/main/java/tp04/metier/Action.java
+++ src/main/java/tp04/metier/Action.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+/* push verifier hl */
 package tp04.metier;
 
 import java.util.Objects;
